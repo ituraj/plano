4b504e4a2e7650a3c1c153b5ce490d7f47f8ec95
Izabela Turaj <i.turaj@gmail.com>
2019-07-16 20:08:13 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      LOL
+    <BrowserRouter>
+      <div className="App">
+        LOL
     </div>
+    </BrowserRouter>
+
   );
 }
 
